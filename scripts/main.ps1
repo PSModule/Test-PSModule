77d15f0e7e62cb563cd724ba1499911b3192f217
Marius Storhaug <marstor@hotmail.com>
2023-08-30 20:47:24 +0200

    Update tasks

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -8,5 +8,8 @@ $Task = ($MyInvocation.MyCommand.Name).split('.')[0]
 Write-Verbose "$Task`: Starting..."
 
 Write-Verbose "$Task`: Message: $Message"
+Write-Verbose "$Task`: Run Pester tests"
+Write-Verbose "$Task`: Run PSScriptAnalyzer"
+Write-Verbose "$Task`: Run linter on docs files"
 
 Write-Verbose "$Task`: Stopping..."
