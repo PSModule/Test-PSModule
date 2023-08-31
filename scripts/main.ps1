e05b598debe6510969908ec17ef165e654f37c41
Marius Storhaug <marstor@hotmail.com>
2023-08-31 19:36:42 +0200

    temp

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,16 +1,17 @@
 ﻿[CmdletBinding()]
 param(
     [Parameter(Mandatory)]
-    [string] $Message
+    [string] $Path
 )
 $Task = ($MyInvocation.MyCommand.Name).split('.')[0]
 
 Write-Verbose "$Task`: Starting..."
+Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force -Verbose:$false
+Install-Module -Name Pester -Scope CurrentUser -Force -Verbose:$false
 
-Write-Verbose "$Task`: Message: $Message"
+Write-Verbose "$Task`: Message: $Path"
 Write-Verbose "$Task`: Run Pester tests"
 Get-ChildItem -Recurse | Select-Object -ExpandProperty FullName | Sort-Object
 Write-Verbose "$Task`: Run PSScriptAnalyzer"
 Write-Verbose "$Task`: Run linter on docs files"
-
 Write-Verbose "$Task`: Stopping..."
