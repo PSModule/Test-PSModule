9f941eab5dc1e9ed00bb582183bb7da215a89d30
Marius Storhaug <marstor@hotmail.com>
2023-09-02 12:25:05 +0200

    add some info

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -8,6 +8,11 @@ Write-Verbose "$Task`: Starting..."
 #Install-Module -Name Pester -Scope CurrentUser -Force -Verbose:$false
 
 Write-Verbose "$Task`: Message: $Path"
+# Import the module using Import-Module $moduleManifestFilePath,
+# Do not not just add the outputted module file to the PATH of the runner (current context is enough) $env:PATH += ";.\outputs\$moduleName" as the import-module will actually test that the module is importable.
+Write-Verbose "$Task`: Test module manifest"
+# Run tests from "".\tests\$moduleName.Tests.ps1"
+Test-ModuleManifest -Path $Path
 Write-Verbose "$Task`: Run Pester tests"
 Get-ChildItem -Recurse | Select-Object -ExpandProperty FullName | Sort-Object
 Write-Verbose "$Task`: Run PSScriptAnalyzer"
