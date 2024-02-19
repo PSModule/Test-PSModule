27141f502d58e2b583641e469392291943bed5dc
Marius Storhaug <marstor@hotmail.com>
2024-02-19 01:18:36 +0100

    Fix

--- outputs/module/PSModuleTest/PSModuleTest.psm1
+++ outputs/module/PSModuleTest/PSModuleTest.psm1
@@ -1,6 +1,14 @@
 ﻿Function Test-PSModuleTest {
+    <#
+        .SYNOPSIS
+        Performs tests on a module.
+
+        .EXAMPLE
+        Test-PSModuleTest -Name 'World'
+    #>
     [CmdletBinding()]
     param (
+        # Name of the person to greet.
         [Parameter(Mandatory)]
         [string]$Name
     )
