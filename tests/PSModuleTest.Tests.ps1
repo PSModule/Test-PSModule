8dd4aa73ac47eeeb08bc912718f2b8f58ee18ae9
Marius Storhaug <marstor@hotmail.com>
2024-02-19 16:24:55 +0100

    Add custom tests

---
+++ tests/PSModuleTest.Tests.ps1
@@ -0,0 +1,9 @@
+﻿Describe "PSModuleTest.Tests.ps1" {
+    It "Should be able to import the module" {
+        Import-Module -Name PSModuleTest -Force | Should -Be $true
+    }
+    It "Should be able to call the function" {
+        $result = Get-PSModuleTest
+        $result | Should -Be "Hello, World!"
+    }
+}
