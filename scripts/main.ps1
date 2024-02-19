030b98bdc067a50cf6053592bb290343394e556a
Marius Storhaug <marstor@hotmail.com>
2024-02-19 15:53:13 +0100

    test verbose

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -14,6 +14,7 @@ if (-not (Test-Path -Path $codeToTest)) {
 try {
     $params = @{
         Path = $codeToTest
+        Verbose = $env:Verbose -eq 'true'
     }
     Invoke-PSModuleTest @params
 } catch {
