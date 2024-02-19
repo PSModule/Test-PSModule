f6d4179e2b536619dbc23206a7f6c096fcd6bb92
Marius Storhaug <marstor@hotmail.com>
2024-02-19 16:54:44 +0100

    Test

--- tests/PSModuleTest.Tests.ps1
+++ tests/PSModuleTest.Tests.ps1
@@ -1,6 +1,7 @@
 ﻿Describe "PSModuleTest.Tests.ps1" {
     It "Should be able to import the module" {
-        Import-Module -Name 'PSModuleTest' -Force | Should -Be $true
+        Import-Module -Name 'PSModuleTest'
+        Get-Module -Name 'PSModuleTest' | Should -Not -BeNullOrEmpty
     }
     It "Should be able to call the function" {
         Test-PSModuleTest -Name 'World' | Should -Be "Hello, World!"
