dfce7fbb6f8eecaa776889125c011957886275b8
Marius Storhaug <marstor@hotmail.com>
2024-02-19 16:56:29 +0100

    Test

--- tests/PSModuleTest.Tests.ps1
+++ tests/PSModuleTest.Tests.ps1
@@ -1,9 +1,11 @@
 ﻿Describe "PSModuleTest.Tests.ps1" {
     It "Should be able to import the module" {
         Import-Module -Name 'PSModuleTest'
+        Write-Verbose (Get-Module -Name 'PSModuleTest' | Out-String) -Verbose
         Get-Module -Name 'PSModuleTest' | Should -Not -BeNullOrEmpty
     }
     It "Should be able to call the function" {
+        Write-Verbose (Test-PSModuleTest -Name 'World' | Out-String) -Verbose
         Test-PSModuleTest -Name 'World' | Should -Be "Hello, World!"
     }
 }
