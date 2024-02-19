e10704412532f70b2f1a6f82d59aaf2be350062e
Marius Storhaug <marstor@hotmail.com>
2024-02-19 16:59:46 +0100

    test

--- tests/PSModuleTest.Tests.ps1
+++ tests/PSModuleTest.Tests.ps1
@@ -1,6 +1,6 @@
 ﻿Describe "PSModuleTest.Tests.ps1" {
     It "Should be able to import the module" {
-        Import-Module -Name 'PSModuleTest'
+        Import-Module -Name 'PSModuleTest' -Verbose:$false
         Write-Verbose (Get-Module -Name 'PSModuleTest' -Verbose:$false | Out-String) -Verbose
         Get-Module -Name 'PSModuleTest' | Should -Not -BeNullOrEmpty
     }
