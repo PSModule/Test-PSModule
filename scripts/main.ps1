b160e36b37a6fc1b2886b7473f08caef0782c725
Marius Storhaug <marstor@hotmail.com>
2024-02-23 00:07:05 +0100

    Fix

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -5,7 +5,7 @@ Get-ChildItem -Path (Join-Path $env:GITHUB_ACTION_PATH 'scripts' 'helpers') -Fil
 }
 Write-Output '##[endgroup]'
 
-$moduleName = [string]::IsNullOrEmpty($env:Name) ? $env:GITHUB_REPOSITORY -replace '.+/', '' : $env:Name
+$moduleName = [string]::IsNullOrEmpty($env:Name) ? $env:GITHUB_REPOSITORY -replace '.+/' : $env:Name
 $codeToTest = Join-Path $env:GITHUB_WORKSPACE $env:Path $moduleName
 if (-not (Test-Path -Path $codeToTest)) {
     throw "Module path [$codeToTest] does not exist."
