260c7d7145e33d67a305ef103d6060c7a68f868a
Marius Storhaug <marstor@hotmail.com>
2024-02-23 23:04:06 +0100

    Update

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,15 +1,13 @@
 ﻿$VerbosePreference = 'Continue'
 Install-PSResource -Name 'Utilities', 'powershell-yaml' -TrustRepository
 
-Write-Output '##[group]Loading helper scripts'
-Get-ChildItem -Path (Join-Path $env:GITHUB_ACTION_PATH 'scripts' 'helpers') -Filter '*.ps1' -Recurse | ForEach-Object {
-    Write-Host "[$($_.FullName)]"
-    . $_.FullName
-}
-Write-Output '##[endgroup]'
+Start-LogGroup 'Loading helper scripts'
+Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts' 'helpers') -Filter '*.ps1' -Recurse |
+    ForEach-Object { Write-Verbose "[$($_.FullName)]"; . $_.FullName }
+Stop-LogGroup
 
-$moduleName = [string]::IsNullOrEmpty($env:Name) ? $env:GITHUB_REPOSITORY -replace '.+/' : $env:Name
-$codeToTest = Join-Path $env:GITHUB_WORKSPACE $env:Path $moduleName
+$moduleName = ($env:Name | IsNullOrEmpty) ? $env:GITHUB_REPOSITORY -replace '.+/' : $env:Name
+$codeToTest = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:Path $moduleName
 if (-not (Test-Path -Path $codeToTest)) {
     throw "Module path [$codeToTest] does not exist."
 }
