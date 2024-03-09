b0d1fd20f7407aae9ae6703990b6d2722a039cb0
Marius Storhaug <marstor@hotmail.com>
2024-03-09 14:33:54 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# test
-Action that is used to test PowerShell modules
+# Test-PSModule
+
+Test PowerShell modules with Pester and PSScriptAnalyzer.
+
+This GitHub Action is a part of the [PSModule framework](https://github.com/PSModule). It is recommended to use the [Process-PSModule workflow](https://github.com/PSModule/Process-PSModule) to automate the whole process of managing the PowerShell module.
+
+## Tools
+
+- [PSScriptAnalyzer | PS Gallery](https://www.powershellgallery.com/packages/PSScriptAnalyzer/)
+- [Test-ModuleManifest - PowerShell Core | Microsoft Learn](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/test-modulemanifest)
+- [PowerShellGet | Microsoft Learn](https://learn.microsoft.com/en-us/powershell/module/PowerShellGet/test-scriptfileinfo)
+- [pester.dev](https://www.pester.dev)
+- [Pester | GitHub](https://github.com/Pester/Pester)
---
+++ scripts/main.ps1
@@ -0,0 +1,47 @@
+﻿#REQUIRES -Modules Utilities
+
+[CmdletBinding()]
+param()
+
+Start-LogGroup 'Loading helper scripts'
+Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts' 'helpers') -Filter '*.ps1' -Recurse |
+    ForEach-Object { Write-Verbose "[$($_.FullName)]"; . $_.FullName }
+Stop-LogGroup
+
+Start-LogGroup 'Loading inputs'
+$moduleName = ($env:GITHUB_ACTION_INPUT_Name | IsNullOrEmpty) ? $env:GITHUB_REPOSITORY_NAME : $env:GITHUB_ACTION_INPUT_Name
+Write-Verbose "Module name:       [$moduleName]"
+
+$codeToTest = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:GITHUB_ACTION_INPUT_Path $moduleName
+Write-Verbose "Code to test:      [$codeToTest]"
+if (-not (Test-Path -Path $codeToTest)) {
+    throw "Path [$codeToTest] does not exist."
+}
+$runModuleTests = $env:GITHUB_ACTION_INPUT_RunModuleTests -eq 'true'
+Write-Verbose "Run module tests:  [$runModuleTests]"
+
+Stop-LogGroup
+
+$params = @{
+    Path = $codeToTest
+    RunModuleTests = $runModuleTests
+}
+$results = Test-PSModule @params
+
+Start-LogGroup 'Test results'
+Write-Verbose ($results | Out-String)
+Stop-LogGroup
+
+$failedTests = $results.FailedCount
+
+if ($failedTests -gt 0) {
+    Write-Output '::error::❌ Some tests failed.'
+    exit $failedTests
+}
+if ($results.Result -ne 'Passed') {
+    Write-Output '::error::❌ Some tests failed.'
+    exit 1
+}
+if ($failedTests -eq 0) {
+    Write-Output '::notice::✅ All tests passed.'
+}
