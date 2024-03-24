b123066440ebe6bf129b5c11a83707bf3d1e9ca4
Marius Storhaug <marstor@hotmail.com>
2024-03-24 23:38:01 +0100

    TEst

--- scripts/helpers/Test-PSModule.ps1
+++ scripts/helpers/Test-PSModule.ps1
@@ -87,13 +87,9 @@ function Test-PSModule {
     #endregion
 
     #region Import module
-    if ((Test-Path -Path $moduleTestsPath) -and $RunModuleTests) {
-        Start-LogGroup "Importing module: $moduleName"
-        Add-PSModulePath -Path (Split-Path $Path -Parent)
-        Get-Module -Name $moduleName -ListAvailable | Remove-Module -Force
-        Import-Module -Name $moduleName -Force -RequiredVersion 999.0.0
-        Stop-LogGroup
-    }
+    Start-LogGroup "Prepare module: $moduleName"
+    Add-PSModulePath -Path (Split-Path $Path -Parent)
+    Stop-LogGroup
     #endregion
 
     #region Pester config
