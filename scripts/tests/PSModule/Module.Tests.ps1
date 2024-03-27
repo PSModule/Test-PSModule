e6a9f94f4de93fa53091b66719df99abaed9386a
Marius Storhaug <marstor@hotmail.com>
2024-03-27 12:39:52 +0100

    test

--- scripts/tests/PSModule/Module.Tests.ps1
+++ scripts/tests/PSModule/Module.Tests.ps1
@@ -20,17 +20,17 @@ Describe 'PSModule - Module tests' {
     Context "Module Manifest" {
         BeforeAll {
             $moduleManifestPath = Join-Path -Path $Path -ChildPath "$moduleName.psd1"
-            Write-Verbose "Module Manifest Path: [$moduleManifestPath]"
         }
         It 'Module Manifest exists' {
+            Write-Verbose "Module Manifest Path: [$moduleManifestPath]"
             $result = Test-Path -Path $moduleManifestPath
-            $result | Should -Be $true
             Write-Verbose $result
+            $result | Should -Be $true
         }
         It 'Module Manifest is valid' {
             $result = Test-ModuleManifest -Path $moduleManifestPath
-            $result | Should -Not -Be $null
             Write-Verbose $result
+            $result | Should -Not -Be $null
         }
         # It 'has a valid license URL' {}
         # It 'has a valid project URL' {}
