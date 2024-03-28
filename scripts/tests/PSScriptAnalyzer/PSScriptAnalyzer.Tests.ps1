ce419820cba91f601c77c5e73c195e946dce7a82
Marius Storhaug <marstor@hotmail.com>
2024-03-28 19:54:48 +0100

    fix

--- scripts/tests/PSScriptAnalyzer/PSScriptAnalyzer.Tests.ps1
+++ scripts/tests/PSScriptAnalyzer/PSScriptAnalyzer.Tests.ps1
@@ -42,7 +42,7 @@ Describe "PSScriptAnalyzer tests using settings file [$relativeSettingsFilePath]
         Write-Warning "Found [$($testResults.Count)] issues"
     }
 
-    Context 'Severity: [<_>]' -ForEach 'Error', 'Warning', 'Information' {
+    Context 'Severity: <_>' -ForEach 'Error', 'Warning', 'Information' {
         It '<CommonName> (<RuleName>)' -ForEach ($rules | Where-Object -Property Severity -EQ $_) {
             $issues = [Collections.Generic.List[string]]::new()
             $testResults | Where-Object -Property RuleName -EQ $RuleName | ForEach-Object {
