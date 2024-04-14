a1399c43e46cd204a43658b9040d1bfaf087e4fc
Marius Storhaug <marstor@hotmail.com>
2024-04-14 22:27:50 +0200

    test

--- scripts/tests/PSModule/SourceCode.Tests.ps1
+++ scripts/tests/PSModule/SourceCode.Tests.ps1
@@ -139,6 +139,7 @@ Describe 'PSModule - SourceCode tests' {
                 [System.Management.Automation.Language.Parser]::ParseFile($FilePath, [ref]$tokens, [ref]$errors)
 
                 foreach ($token in $tokens) {
+                    Write-Verbose ($token | Out-String) -Verbose
                     $keyword = $token.TokenFlags.Text
                     $lineNumber = $token.Extent.StartLineNumber
                     $columnNumber = $token.Extent.StartColumnNumber
