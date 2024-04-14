692b7fef9708d16124990a891974ef3820f78230
Marius Storhaug <marstor@hotmail.com>
2024-04-14 11:11:25 +0200

    Test

--- tests/src/init/initializer.ps1
+++ tests/src/init/initializer.ps1
@@ -1,3 +1,3 @@
-﻿Write-Verbose '-------------------------------'
-Write-Verbose '---  THIS IS AN INITIALIZER ---'
-Write-Verbose '-------------------------------'
+﻿Write-Verbose '-------------------------------' -Verbose -ErrorAction Stop
+Write-Verbose '---  THIS IS AN INITIALIZER ---' -Verbose:$true -ErrorAction Stop
+Write-Verbose '-------------------------------' -Verbose:$false -ErrorAction Stop
