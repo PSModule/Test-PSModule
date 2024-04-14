ec8d625b5f27e4f3b2dd87e62146f4135958c8e3
Marius Storhaug <marstor@hotmail.com>
2024-04-14 19:47:08 +0200

    Check for param() block

--- tests/src/public/Get-PSModuleTest.ps1
+++ tests/src/public/Get-PSModuleTest.ps1
@@ -10,11 +10,5 @@ function Get-PSModuleTest {
 
         "Hello, World!"
     #>
-    [CmdletBinding()]
-    param (
-        # Name of the person to greet.
-        [Parameter(Mandatory)]
-        [string] $Name
-    )
-    Write-Output "Hello, $Name!"
+    Write-Output "Hello, World!"
 }
