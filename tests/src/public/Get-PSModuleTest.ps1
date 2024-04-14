5afade1d61717239206539cfc048ef8637cd23f3
Marius Storhaug <marstor@hotmail.com>
2024-04-14 18:29:54 +0200

    File should only contain one function

--- tests/src/public/Get-PSModuleTest.ps1
+++ tests/src/public/Get-PSModuleTest.ps1
@@ -18,3 +18,22 @@ function Get-PSModuleTest {
     )
     Write-Output "Hello, $Name!"
 }
+
+filter Get-PSModuleTestfilter {
+    <#
+        .SYNOPSIS
+        Performs tests on a module.
+
+        .EXAMPLE
+        Test-PSModule -Name 'World'
+
+        "Hello, World!"
+    #>
+    [CmdletBinding()]
+    param (
+        # Name of the person to greet.
+        [Parameter(Mandatory)]
+        [string] $Name
+    )
+    Write-Output "Hello, $Name!"
+}
