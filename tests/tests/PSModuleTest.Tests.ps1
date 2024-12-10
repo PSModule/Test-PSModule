dc706e2c4719cfb28d4225d049eec9e0090398da
Marius Storhaug <marstor@hotmail.com>
2024-12-10 23:24:10 +0100

    test

--- tests/tests/PSModuleTest.Tests.ps1
+++ tests/tests/PSModuleTest.Tests.ps1
@@ -1,17 +1,13 @@
 ﻿
 Describe 'Module' {
-    BeforeEach {
-        $DebugPreference = 'Continue'
-    }
-
     It 'Function: Get-PSModuleTest' {
-        Get-PSModuleTest -Name 'World' -Verbose | Should -Be 'Hello, World!'
+        Get-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
     }
     It 'Function: New-PSModuleTest' {
         New-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
     }
     It 'Function: Set-PSModuleTest' {
-        Set-PSModuleTest -Name 'World' -Verbose | Should -Be 'Hello, World!'
+        Set-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
     }
     It 'Function: Test-PSModuleTest' {
         Test-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
