fe8b2bf14c7c02bd669d8110090aaef4a8a4b158
Marius Storhaug <marstor@hotmail.com>
2024-12-21 16:58:22 +0100

    🚀 [Feature]: Enable skips on SourceCode.PSModule tests (#84)
    ## Description
    
    - Implements a inline way to skip tests on source code tests.
      - Fixes #73
    - Allow to hold completers in the `src/functions/public` folder.
    - Improve gathering of info from the source code.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [x] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/src/functions/public/New-PSModuleTest.ps1
+++ tests/src/functions/public/New-PSModuleTest.ps1
@@ -1,5 +1,6 @@
 ﻿#Requires -Modules @{ModuleName='PSSemVer'; ModuleVersion='1.0'}
-
+#SkipTest:FunctionTest:Difficult to test due to the nature of the function.
+#SkipTest:Verbose:Just want to test that a function can have multiple skips.
 function New-PSModuleTest {
     <#
         .SYNOPSIS
@@ -28,11 +29,10 @@ function New-PSModuleTest {
         [string] $Name
     )
     Write-Debug "Debug message"
+    Write-Verbose "Verbose message" -Verbose
     Write-Output "Hello, $Name!"
 }
 
 New-Alias New-PSModuleTestAlias3 New-PSModuleTest
 New-Alias -Name New-PSModuleTestAlias4 -Value New-PSModuleTest
-
-
 Set-Alias New-PSModuleTestAlias5 New-PSModuleTest
