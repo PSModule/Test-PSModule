70bc40eaa6e2b06958eef62e46767c0a0f99ed86
Marius Storhaug <marstor@hotmail.com>
2025-02-20 16:22:54 +0100

    Enhance: Update path resolution in test scripts to improve consistency and reliability

--- scripts/tests/Module/PSModule/Module.Container.ps1
+++ scripts/tests/Module/PSModule/Module.Container.ps1
@@ -1,7 +1,7 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path    = Resolve-Path -Path $env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path | Select-Object -ExpandProperty Path
+        Path    = Resolve-Path -Path "$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path" | Select-Object -ExpandProperty Path
         Debug   = $false
         Verbose = $false
     }
