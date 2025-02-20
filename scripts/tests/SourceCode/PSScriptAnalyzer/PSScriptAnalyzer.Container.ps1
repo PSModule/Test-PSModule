44aa2d151ea71492057d87ee0856713d9627cdfa
Marius Storhaug <marstor@hotmail.com>
2025-02-20 17:10:16 +0100

    Enhance: Simplify path assignment in container scripts for improved clarity and consistency

--- scripts/tests/SourceCode/PSScriptAnalyzer/PSScriptAnalyzer.Container.ps1
+++ scripts/tests/SourceCode/PSScriptAnalyzer/PSScriptAnalyzer.Container.ps1
@@ -1,7 +1,7 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path             = "$env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_Run_Path"
+        Path             = $env:GITHUB_ACTION_INPUT_Run_Path
         SettingsFilePath = Resolve-Path -Path "$PSScriptRoot/PSScriptAnalyzer.Settings.psd1"
         Debug            = $false
         Verbose          = $false
