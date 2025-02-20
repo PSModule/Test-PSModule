4fbe6d0db05e9cbd269d9fd920a47512b642fca5
Marius Storhaug <marstor@hotmail.com>
2025-02-20 16:54:27 +0100

    Enhance: Simplify path resolution in test scripts for improved clarity and consistency

--- scripts/tests/Module/PSScriptAnalyzer/PSScriptAnalyzer.Container.ps1
+++ scripts/tests/Module/PSScriptAnalyzer/PSScriptAnalyzer.Container.ps1
@@ -1,7 +1,7 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path             = Resolve-Path -Path $env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path | Select-Object -ExpandProperty Path
+        Path             = "$env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path"
         SettingsFilePath = Resolve-Path -Path "$PSScriptRoot/PSScriptAnalyzer.Settings.psd1"
         Debug            = $false
         Verbose          = $false
--- scripts/tests/SourceCode/PSModule/SourceCode.Container.ps1
+++ scripts/tests/SourceCode/PSModule/SourceCode.Container.ps1
@@ -1,8 +1,8 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path      = Resolve-Path -Path "$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path" | Select-Object -ExpandProperty Path
-        TestsPath = Resolve-Path -Path "$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path/tests" | Select-Object -ExpandProperty Path
+        Path      = "$env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path"
+        TestsPath = "$env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path/tests"
         Debug     = $false
         Verbose   = $false
     }
