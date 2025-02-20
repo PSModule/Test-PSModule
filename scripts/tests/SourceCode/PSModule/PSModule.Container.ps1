ff37be836d77101d2632bf522fc2658c62b6e95c
Marius Storhaug <marstor@hotmail.com>
2025-02-20 17:06:29 +0100

    Enhance: Update path variables in container scripts for consistency in test execution

--- scripts/tests/SourceCode/PSModule/PSModule.Container.ps1
+++ scripts/tests/SourceCode/PSModule/PSModule.Container.ps1
@@ -1,8 +1,8 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path      = "$env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path"
-        TestsPath = "$env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path/tests"
+        Path      = "$env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_Run_Path"
+        TestsPath = "$env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_Run_Path/tests"
         Debug     = $false
         Verbose   = $false
     }
