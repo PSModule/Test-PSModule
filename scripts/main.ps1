9e271dff9d8fb608158febeacd91f8ca65610aab
Marius Storhaug <marstor@hotmail.com>
2025-02-20 23:21:12 +0100

    Refactor: Simplify code path resolution in main.ps1 and update test paths in container scripts for consistency

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -2,17 +2,18 @@
 $moduleName = [string]::IsNullOrEmpty($env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Name) ? $env:GITHUB_REPOSITORY_NAME : $env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Name
 $testType = $env:GITHUB_ACTION_INPUT_TEST_PSMODULE_TestType
 $testPath = Resolve-Path -Path "$PSScriptRoot/tests/$testType" | Select-Object -ExpandProperty Path
-$codePath = switch ($testType) {
-    'Module' {
-        Resolve-Path -Path "$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path/outputs/modules/$moduleName" | Select-Object -ExpandProperty Path
-    }
-    'SourceCode' {
-        Resolve-Path -Path $env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path/src | Select-Object -ExpandProperty Path
-    }
-    default {
-        throw "Invalid test type: [$testType]"
-    }
-}
+$codePath = $env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path
+# switch ($testType) {
+#     'Module' {
+#         Resolve-Path -Path "$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path/outputs/modules/$moduleName" | Select-Object -ExpandProperty Path
+#     }
+#     'SourceCode' {
+#         Resolve-Path -Path "$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path/src" | Select-Object -ExpandProperty Path
+#     }
+#     default {
+#         throw "Invalid test type: [$testType]"
+#     }
+# }
 
 [pscustomobject]@{
     ModuleName = $moduleName
--- scripts/tests/SourceCode/PSModule/PSModule.Container.ps1
+++ scripts/tests/SourceCode/PSModule/PSModule.Container.ps1
@@ -1,7 +1,7 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path      = $env:GITHUB_ACTION_INPUT_Run_Path
+        Path      = "$env:GITHUB_ACTION_INPUT_Run_Path/src"
         TestsPath = "$env:GITHUB_ACTION_INPUT_Run_Path/tests"
         Debug     = $false
         Verbose   = $false
