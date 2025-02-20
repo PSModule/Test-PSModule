51baa1424ed33514f4eac7c4182b2f90b24902fd
Marius Storhaug <marstor@hotmail.com>
2025-02-20 16:58:36 +0100

    Enhance: Refactor test scripts by consolidating and updating PSModule.Container.ps1 and related files for improved structure and clarity

--- scripts/tests/Module/PSScriptAnalyzer/PSScriptAnalyzer.Container.ps1
+++ scripts/tests/Module/PSScriptAnalyzer/PSScriptAnalyzer.Container.ps1
@@ -2,7 +2,7 @@
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
         Path             = "$env:GITHUB_WORKSPACE/$env:GITHUB_ACTION_INPUT_TEST_PSMODULE_Path"
-        SettingsFilePath = Resolve-Path -Path "$PSScriptRoot/PSScriptAnalyzer.Settings.psd1"
+        SettingsFilePath = "$PSScriptRoot/PSScriptAnalyzer.Settings.psd1"
         Debug            = $false
         Verbose          = $false
     }
