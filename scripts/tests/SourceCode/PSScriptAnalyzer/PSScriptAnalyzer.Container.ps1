191938913f54272858b3364015d12cf78382a164
Marius Storhaug <marstor@hotmail.com>
2025-02-23 22:23:43 +0100

    Refactor: Update Invoke-Pester action to version 3 and remove obsolete PSScriptAnalyzer files

--- scripts/tests/SourceCode/PSScriptAnalyzer/PSScriptAnalyzer.Container.ps1
+++
@@ -1,9 +0,0 @@
-﻿@{
-    Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
-    Data = @{
-        Path             = "$env:GITHUB_ACTION_INPUT_Run_Path" | Get-Item
-        SettingsFilePath = "$PSScriptRoot/PSScriptAnalyzer.Settings.psd1" | Get-Item
-        Debug            = $false
-        Verbose          = $false
-    }
-}
