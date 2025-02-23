32377f38475e7827417e33e83cab56a52aef15df
Marius Storhaug <marstor@hotmail.com>
2025-02-24 00:49:12 +0100

    Refactor: Update path resolution for module outputs in PSModule.Container scripts

--- scripts/tests/Module/PSModule/PSModule.Container.ps1
+++ scripts/tests/Module/PSModule/PSModule.Container.ps1
@@ -1,7 +1,7 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path    = $env:GITHUB_ACTION_INPUT_Run_Path
+        Path    = Resolve-Path "$env:GITHUB_ACTION_INPUT_Run_Path/outputs/modules/$moduleName" | Select-Object -ExpandProperty Path
         Debug   = $false
         Verbose = $false
     }
--- scripts/tests/SourceCode/PSModule/PSModule.Container.ps1
+++ scripts/tests/SourceCode/PSModule/PSModule.Container.ps1
@@ -1,7 +1,7 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path      = $env:GITHUB_ACTION_INPUT_Run_Path
+        Path      = Resolve-Path "$env:GITHUB_ACTION_INPUT_Run_Path/src" | Select-Object -ExpandProperty Path
         TestsPath = Resolve-Path $env:GITHUB_ACTION_INPUT_Run_Path/../tests | Select-Object -ExpandProperty Path
         Debug     = $false
         Verbose   = $false
