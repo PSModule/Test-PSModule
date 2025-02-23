e3bfac1d49ea47c4dbecffe4cd6a806ce76a3363
Marius Storhaug <marstor@hotmail.com>
2025-02-23 23:20:27 +0100

    Refactor: Remove TestSuiteName from Action-Test workflows and simplify path handling in PSModule.Container scripts

--- scripts/tests/Module/PSModule/PSModule.Container.ps1
+++ scripts/tests/Module/PSModule/PSModule.Container.ps1
@@ -1,7 +1,7 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path    = "$env:GITHUB_ACTION_INPUT_Run_Path/output/modules/$env:ModuleName"
+        Path    = $env:GITHUB_ACTION_INPUT_Run_Path
         Debug   = $false
         Verbose = $false
     }
--- scripts/tests/SourceCode/PSModule/PSModule.Container.ps1
+++ scripts/tests/SourceCode/PSModule/PSModule.Container.ps1
@@ -1,8 +1,8 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path      = "$env:GITHUB_ACTION_INPUT_Run_Path" | Get-Item
-        TestsPath = "$env:GITHUB_ACTION_INPUT_Run_Path/../tests" | Get-Item
+        Path      = $env:GITHUB_ACTION_INPUT_Run_Path
+        TestsPath = Resolve-Path $env:GITHUB_ACTION_INPUT_Run_Path/../tests | Select-Object -ExpandProperty Path
         Debug     = $false
         Verbose   = $false
     }
