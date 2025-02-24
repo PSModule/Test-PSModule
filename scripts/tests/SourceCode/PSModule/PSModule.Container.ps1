18285b7650f622c9e297e7ca9c76db44560c8524
Marius Storhaug <marstor@hotmail.com>
2025-02-24 02:39:13 +0100

    Refactor: Update test path handling in action workflow and scripts for improved clarity and functionality

--- scripts/tests/SourceCode/PSModule/PSModule.Container.ps1
+++ scripts/tests/SourceCode/PSModule/PSModule.Container.ps1
@@ -1,8 +1,8 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path      = Resolve-Path -Path "$env:GITHUB_ACTION_INPUT_Run_Path/src" | Select-Object -ExpandProperty Path
-        TestsPath = Resolve-Path -Path "$env:GITHUB_ACTION_INPUT_Run_Path/tests" | Select-Object -ExpandProperty Path
+        Path      = $env:GITHUB_ACTION_INPUT_Run_Path
+        TestsPath = $env:LocalTestPath
         Debug     = $false
         Verbose   = $false
     }
