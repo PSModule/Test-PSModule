739246d31e54c8a2afb0d716f8007ace5cafa949
Marius Storhaug <marstor@hotmail.com>
2025-02-28 02:36:20 +0100

    Add WorkingDirectory input to action.yml and update script paths accordingly

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -5,28 +5,31 @@ $moduleName = if ([string]::IsNullOrEmpty($env:PSMODULE_TEST_PSMODULE_INPUT_Name
     $env:PSMODULE_TEST_PSMODULE_INPUT_Name
 }
 $settings = $env:PSMODULE_TEST_PSMODULE_INPUT_Settings
+$workingDirectory = $env:PSMODULE_TEST_PSMODULE_INPUT_WorkingDirectory
 $testPath = Resolve-Path -Path "$PSScriptRoot/tests/$settings" | Select-Object -ExpandProperty Path
-$localTestPath = Resolve-Path -Path "$env:PSMODULE_TEST_PSMODULE_INPUT_Path/tests" | Select-Object -ExpandProperty Path
+$localTestPath = Resolve-Path -Path (Join-Path -Path $workingDirectory -ChildPath "tests") | Select-Object -ExpandProperty Path
 $codePath = switch ($settings) {
     'Module' {
-        Resolve-Path -Path "$env:PSMODULE_TEST_PSMODULE_INPUT_Path/outputs/module/$moduleName" | Select-Object -ExpandProperty Path
+        Resolve-Path -Path (Join-Path -Path $workingDirectory -ChildPath "outputs/module/$moduleName") | Select-Object -ExpandProperty Path
     }
     'SourceCode' {
-        Resolve-Path -Path "$env:PSMODULE_TEST_PSMODULE_INPUT_Path/src" | Select-Object -ExpandProperty Path
+        Resolve-Path -Path (Join-Path -Path $workingDirectory -ChildPath "src") | Select-Object -ExpandProperty Path
     }
     default {
         throw "Invalid test type: [$settings]"
     }
 }
 [pscustomobject]@{
-    ModuleName    = $moduleName
-    Settings      = $settings
-    CodePath      = $codePath
-    LocalTestPath = $localTestPath
-    TestPath      = $testPath
+    ModuleName      = $moduleName
+    Settings        = $settings
+    CodePath        = $codePath
+    LocalTestPath   = $localTestPath
+    TestPath        = $testPath
+    WorkingDirectory = $workingDirectory
 } | Format-List
 
 Set-GitHubOutput -Name ModuleName -Value $moduleName
 Set-GitHubOutput -Name CodePath -Value $codePath
 Set-GitHubOutput -Name LocalTestPath -Value $localTestPath
 Set-GitHubOutput -Name TestPath -Value $testPath
+Set-GitHubOutput -Name WorkingDirectory -Value $workingDirectory
--- scripts/tests/Module/PSModule/PSModule.Container.ps1
+++ scripts/tests/Module/PSModule/PSModule.Container.ps1
@@ -1,7 +1,7 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path    = $env:GITHUB_ACTION_INPUT_Run_Path
+        Path    = $env:PSMODULE_INVOKE_PESTER_INPUT_Run_Path
         Debug   = $false
         Verbose = $false
     }
