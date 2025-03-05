22526b5c92f891f839ea2aad34ae18d3d37aec70
Marius Storhaug <marstor@hotmail.com>
2025-03-05 15:25:55 +0100

    Refactor module path handling in main.ps1 to improve clarity and consistency

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -17,14 +17,14 @@ $testPath = Resolve-Path -Path "$PSScriptRoot/tests/$settings" | Select-Object -
 $localTestPath = Resolve-Path -Path 'tests' | Select-Object -ExpandProperty Path
 switch ($settings) {
     'Module' {
-        $codePath = Resolve-Path -Path "outputs/module/$moduleName" | Select-Object -ExpandProperty Path
-        $manifestFilePath = Join-Path -Path $codePath "$moduleName.psd1"
+        $modulePath = Resolve-Path -Path "outputs/module/$moduleName" | Select-Object -ExpandProperty Path
+        $manifestFilePath = Join-Path -Path $modulePath "$moduleName.psd1"
         Write-Verbose " - Manifest file path: [$manifestFilePath]" -Verbose
         Resolve-PSModuleDependency -ManifestFilePath $manifestFilePath
         $PSModulePath = $env:PSModulePath -split [System.IO.Path]::PathSeparator | Select-Object -First 1
-        $moduleInstallPath = New-Item -Path "$PSModulePath/$moduleName/999.0.0" -ItemType Directory -Force
-        Copy-Item -Path $codePath -Destination $moduleInstallPath -Recurse -Force
-        Get-ChildItem -Path $moduleInstallPath -Recurse | Select-Object FullName | Out-String
+        $codePath = New-Item -Path "$PSModulePath/$moduleName/999.0.0" -ItemType Directory -Force
+        Copy-Item -Path $codePath -Destination $codePath -Recurse -Force
+        Get-ChildItem -Path $codePath -Recurse | Select-Object FullName | Out-String
         Import-Module -Name $moduleName -Verbose
     }
     'SourceCode' {
@@ -38,12 +38,12 @@ switch ($settings) {
 [pscustomobject]@{
     ModuleName    = $moduleName
     Settings      = $settings
-    CodePath      = $moduleInstallPath
+    CodePath      = $codePath
     LocalTestPath = $localTestPath
     TestPath      = $testPath
 } | Format-List | Out-String
 
 Set-GitHubOutput -Name ModuleName -Value $moduleName
-Set-GitHubOutput -Name CodePath -Value $moduleInstallPath
+Set-GitHubOutput -Name CodePath -Value $codePath
 Set-GitHubOutput -Name LocalTestPath -Value $localTestPath
 Set-GitHubOutput -Name TestPath -Value $testPath
