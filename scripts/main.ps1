5b296fe239df1343d446341aa04d4cbb64130dc9
Marius Storhaug <marstor@hotmail.com>
2025-03-05 20:08:37 +0100

    Add PSModuleHelpers installation step and remove deprecated helper scripts

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,9 +1,4 @@
-﻿$helperPath = "$PSScriptRoot/helpers"
-Get-ChildItem -Path $helperPath -Filter '*.ps1' -Recurse | ForEach-Object {
-    . $_.FullName
-}
-
-# If test type is module, the code we ought to test is in the WorkingDirectory/outputs/module/Name folder,
+﻿# If test type is module, the code we ought to test is in the WorkingDirectory/outputs/module/Name folder,
 # otherwise it's in the WorkingDirectory/src folder.
 
 $moduleName = if ([string]::IsNullOrEmpty($env:PSMODULE_TEST_PSMODULE_INPUT_Name)) {
@@ -18,15 +13,7 @@ $localTestPath = Resolve-Path -Path 'tests' | Select-Object -ExpandProperty Path
 switch ($settings) {
     'Module' {
         $modulePath = Resolve-Path -Path "outputs/module/$moduleName" | Select-Object -ExpandProperty Path
-        $manifestFilePath = Join-Path -Path $modulePath "$moduleName.psd1"
-        Write-Verbose " - Manifest file path: [$manifestFilePath]" -Verbose
-        Resolve-PSModuleDependency -ManifestFilePath $manifestFilePath
-        $PSModulePath = $env:PSModulePath -split [System.IO.Path]::PathSeparator | Select-Object -First 1
-        $codePath = New-Item -Path "$PSModulePath/$moduleName/999.0.0" -ItemType Directory -Force | Select-Object -ExpandProperty FullName
-        Copy-Item -Path "$modulePath/*" -Destination $codePath -Recurse -Force
-        LogGroup 'Importing module' {
-            Import-Module -Name $moduleName -Verbose
-        }
+        Install-PSModule -Path $modulePath
     }
     'SourceCode' {
         $codePath = Resolve-Path -Path 'src' | Select-Object -ExpandProperty Path
